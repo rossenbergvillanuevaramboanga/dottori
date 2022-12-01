32394047b683af2edbff91eb7487d2d5c7ae7814
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-12-01 22:54:14 +0100

    Implemented Model Dottore

---
+++ src/main/java/it/prova/dottori/model/Dottore.java
@@ -0,0 +1,105 @@
+package it.prova.dottori.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dottore")
+public class Dottore {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "codicedottore")
+	private String codiceDottore;
+	@Column(name = "codicefiscalepaziente")
+	private String codiceFiscalePaziente;
+	@Column(name = "invisita")
+	private Boolean inVisita;
+	@Column(name = "inservizio")
+	private Boolean inServizio;
+	
+	public Dottore() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dottore(Long id, String nome, String cognome, String codiceDottore, String codiceFiscalePaziente,
+			Boolean inVisita, Boolean inServizio) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceDottore = codiceDottore;
+		this.codiceFiscalePaziente = codiceFiscalePaziente;
+		this.inVisita = inVisita;
+		this.inServizio = inServizio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceDottore() {
+		return codiceDottore;
+	}
+
+	public void setCodiceDottore(String codiceDottore) {
+		this.codiceDottore = codiceDottore;
+	}
+
+	public String getCodiceFiscalePaziente() {
+		return codiceFiscalePaziente;
+	}
+
+	public void setCodiceFiscalePaziente(String codiceFiscalePaziente) {
+		this.codiceFiscalePaziente = codiceFiscalePaziente;
+	}
+
+	public Boolean getInVisita() {
+		return inVisita;
+	}
+
+	public void setInVisita(Boolean inVisita) {
+		this.inVisita = inVisita;
+	}
+
+	public Boolean getInServizio() {
+		return inServizio;
+	}
+
+	public void setInServizio(Boolean inServizio) {
+		this.inServizio = inServizio;
+	}
+	
+	
+
+}
