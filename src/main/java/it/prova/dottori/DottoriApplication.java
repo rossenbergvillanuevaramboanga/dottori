e1b8e855c3bd4910cc81a305bbfbc4ee53c17f05
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-12-01 17:00:10 +0100

    first commit

---
+++ src/main/java/it/prova/dottori/DottoriApplication.java
@@ -0,0 +1,13 @@
+package it.prova.dottori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DottoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DottoriApplication.class, args);
+	}
+
+}
