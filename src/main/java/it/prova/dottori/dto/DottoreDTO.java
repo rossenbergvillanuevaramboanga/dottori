484ffa896d7fe7f152339f660bcba3f011dfb37e
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-12-02 15:09:23 +0100

    Fixed code

--- src/main/java/it/prova/dottori/dto/DottoreDTO.java
+++ src/main/java/it/prova/dottori/dto/DottoreDTO.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.validation.constraints.NotBlank;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import it.prova.dottori.model.Dottore;
